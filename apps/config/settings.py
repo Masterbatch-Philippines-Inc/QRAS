c2f034ebe7642b00cfae86644967c28daa7a3a3a
thanyeal <128719578+thanyeal@users.noreply.github.com>
2026-09-12 23:54:40 +0800

    refactor: settings.py
    - removed depreciated email variables

--- apps/config/settings.py
+++ apps/config/settings.py
@@ -155,21 +155,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MAILERS = {
     'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
+        'BACKEND':  config('EMAIL_BACKEND',         default='django.core.mail.backends.smtp.EmailBackend'),
+        'HOST':     config('EMAIL_HOST',            default='smtp.gmail.com'),
+        'PORT':     config('EMAIL_PORT',            default=587, cast=int),
+        'USERNAME': config('EMAIL_HOST_USER',       default='mbpi.itsupport@gmail.com'),
+        'PASSWORD': config('EMAIL_HOST_PASSWORD',   default='secret'),
+        'USE_TLS':  config('EMAIL_USE_TLS',         default=True, cast=bool),
     },
 }
 
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL',   default='mbpi.itsupport@gmail.com')
 
-# Custom Email Settings (for production)
-# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
-
-EMAIL_BACKEND           = config('EMAIL_BACKEND',       default='django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST              = config('EMAIL_HOST',          default='smtp.gmail.com')
-EMAIL_PORT              = config('EMAIL_PORT',          default=587, cast=int)
-EMAIL_USE_TLS           = config('EMAIL_USE_TLS',       default=True, cast=bool)
-EMAIL_HOST_USER         = config('EMAIL_HOST_USER',     default='mbpi.itsupport@gmail.com')
-EMAIL_HOST_PASSWORD     = config('EMAIL_HOST_PASSWORD', default='secret')
-DEFAULT_FROM_EMAIL      = config('DEFAULT_FROM_EMAIL',  default=EMAIL_HOST_USER)
 
 # Seconds -- reset link valid for 1 hour
 PASSWORD_RESET_TIMEOUT  = 3600  
